b61b9ceed0ae975b3cff455927a7e65559539adb
OmBhavsar161 <ombhavsar2004@gmail.com>
2024-09-08 00:03:34 +0530

    bur

--- backend/index.js
+++ backend/index.js
@@ -55,7 +55,7 @@ app.use("/images", express.static("/tmp"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+    image_url: `https://ecom-vercel-backend.vercel.app/images/${req.file.filename}`, // changes
   });
 });
 
