eb8b6ae4ac24479e1a2af55c8d5ca9039dbfc5a5
OmBhavsar161 <ombhavsar2004@gmail.com>
2024-09-08 00:59:24 +0530

    nn

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const port = process.env.PORT || 4000;
+const port = 4000 || process.env.PORT ;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
